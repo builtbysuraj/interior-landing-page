8149f8b28aa3203cc98770b74187f50ce36bfb6d
builtbysuraj <surajgupta3553@gmail.com>
2025-01-09 17:54:58 +0530

    feat: Integrate the API of webhook

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,52 @@
+import { useState } from 'react'
+import toast from 'react-hot-toast'
+
 export default function ContactForm() {
-  const handleFormSubmit = (evt) => {
-    evt.preventDefault()
-    const { name, value } = evt.target
-    console.log({ name }, { value })
+  const URL = 'https://hook.eu2.make.com/fqggdswoqbpf7krk1wbfa352a20be2jm'
+  const [formData, setFormData] = useState({
+    Name: '',
+    Email: '',
+    Phone: '',
+    Description: '',
+  })
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value })
   }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault()
+
+    setIsLoading(true) // Set loading state
+
+    try {
+      const response = await fetch(URL, {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to submit form')
+      }
+
+      setIsSuccess(true)
+      setIsLoading(false)
+      toast.success(
+        "Thank you for contacting us! We'll get back to you shortly."
+      )
+      setFormData({ Name: '', Email: '', Phone: '', Description: '' })
+    } catch (error) {
+      console.error('Error submitting form:', error)
+      toast.error('An error occurred. Please contact us by phone or email.')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <div className='flex-1'>
       <h2 className='font-playfairDisplay italic text-5xl text-center text-primary mb-10'>
@@ -27,6 +70,8 @@ export default function ContactForm() {
                   name='Name'
                   placeholder='Name'
                   required
+                  value={formData.Name}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -44,6 +89,8 @@ export default function ContactForm() {
                   name='Email'
                   placeholder='Email'
                   required
+                  value={formData.Email}
+                  onChange={handleChange}
                 />
               </div>
               <div className='mt-5'>
@@ -60,6 +107,8 @@ export default function ContactForm() {
                   name='Phone'
                   placeholder='Phone number'
                   required
+                  value={formData.Phone}
+                  onChange={handleChange}
                 />
               </div>
               <div className='mt-5'>
@@ -73,13 +122,17 @@ export default function ContactForm() {
                   className='w-full h-32 px-4 py-4 rounded-lg bg-gray-100 border border-gray-400 text-sm focus:outline-none'
                   type='text'
                   id='description'
+                  name='Description'
                   placeholder='Enter your Message'
                   required
+                  value={formData.Description}
+                  onChange={handleChange}
                 />
               </div>
               <button
                 type='submit'
                 className='mt-5 tracking-wide font-semibold bg-primary text-gray-100 w-full py-2 rounded-lg transition-all duration-300 ease-in-out flex items-center justify-center text-lg'
+                disabled={isLoading}
               >
                 <span className='ml-3'>Send</span>
               </button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <Toaster />
     <App />
   </BrowserRouter>
 )
