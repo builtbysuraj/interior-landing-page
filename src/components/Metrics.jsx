48720b339561133a68fe523be140a32aa8f13d2f
builtbysuraj <surajgupta3553@gmail.com>
2025-01-10 13:47:33 +0530

    Add incremental in metrics

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -1,16 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+const Counter = ({ target }) => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    let interval
+    if (count < target) {
+      interval = setInterval(() => {
+        setCount((prevCount) => Math.min(prevCount + 1, target))
+      }, 50) // Adjust speed here
+    }
+    return () => clearInterval(interval)
+  }, [count, target])
+
+  return (
+    <span className='font-playfairDisplay text-5xl md:text-6xl'>{count}</span>
+  )
+}
+
 export default function Metrics() {
   return (
     <section>
       <div className='flex justify-between items-center py-4 flex-wrap gap-7'>
         <div className='flex items-center gap-3'>
-          <span className='font-playfairDisplay text-5xl md:text-6xl'>45</span>
+          <Counter target={45} />
           <span className='flex items-center mt-3 -ml-3 gap-2'>
             <p className='font-playfairDisplay text-5xl md:text-6xl'>+</p>
             <p className='font-outfit text-textDark text-sm mt-2'>PROJECTS</p>
           </span>
         </div>
         <div className='flex items-center gap-3 max-w-[300px]'>
-          <span className='font-playfairDisplay text-5xl md:text-6xl'>15</span>
+          <Counter target={15} />
           <span className='flex items-center mt-3 -ml-3 gap-2'>
             <p className='font-playfairDisplay text-5xl md:text-6xl'>+</p>
             <p className='font-outfit text-textDark text-sm mt-2'>
@@ -19,25 +39,15 @@ export default function Metrics() {
           </span>
         </div>
         <div className='flex items-center gap-3'>
-          <span className='font-playfairDisplay text-5xl md:text-6xl'>20</span>
+          <Counter target={20} />
           <span className='flex items-center mt-3 -ml-3 gap-2'>
             <p className='font-playfairDisplay text-5xl md:text-6xl'>+</p>
             <p className='font-outfit text-textDark text-sm mt-2'>
-              DEDICTED PROFESSIONALS
+              DEDICATED PROFESSIONALS
             </p>
           </span>
         </div>
-        {/* <div className='flex items-center gap-3'>
-          <span className='font-playfairDisplay text-5xl md:text-6xl'>
-            154
-          </span>
-          <p className='font-outfit text-textDark text-sm mt-5'>
-            {' '}
-            TOTAL VISITORS
-          </p>
-        </div> */}
       </div>
-      {/* <hr className='mt-20 text-hrColor' /> */}
       <hr className='my-20 border-gray-300' />
     </section>
   )
